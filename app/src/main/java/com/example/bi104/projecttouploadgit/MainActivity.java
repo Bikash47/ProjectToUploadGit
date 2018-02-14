78cfc972016ee4b94960b5a0e373310633b2f70f
bikash <bikashkumarsahu9@gmail.com>
2018-02-14 19:49:43 +0530

    commiting

--- app/src/main/java/com/example/bi104/projecttouploadgit/MainActivity.java
+++ app/src/main/java/com/example/bi104/projecttouploadgit/MainActivity.java
@@ -10,5 +10,6 @@ TextView tv ;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Checking
     }
 }
